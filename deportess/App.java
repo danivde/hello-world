c56f294a3a8e4edc1b6b2b721f4e3d5b93555930
danivde <dvazquezd9@gmail.com>
2024-12-05 08:18:32 +0100

    agregar clase la Liga

--- deportess/App.java
+++ deportess/App.java
@@ -1,5 +1,8 @@
 package diciembre.deporte;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class App {
     public static void main(String[] args) {
         
@@ -11,6 +14,36 @@ public class App {
         System.out.println(); 
         baloncesto.mostrarDetalles();
     }
+ // Crear una liga
+        Liga ligaFutbol = new Liga("Liga Nacional de Fútbol");
+        
+        // Agregar equipos a la liga
+        ligaFutbol.agregarEquipo("Real Madrid");
+        ligaFutbol.agregarEquipo("Barcelona");
+        ligaFutbol.agregarEquipo("Atlético de Madrid");
+
+        // Mostrar información de la liga y los equipos
+        System.out.println("Liga: " + ligaFutbol.getNombre());
+        System.out.println("Equipos en la liga:");
+        ligaFutbol.mostrarEquipos();
+
+        // Crear instancias de los deportes
+        List<Deporte> deportes = new ArrayList<>();
+        deportes.add(new Futbol());
+        deportes.add(new Baloncesto());
+
+        // Mostrar información de cada deporte
+        System.out.println("\nInformación de deportes:");
+        for (Deporte deporte : deportes) {
+            System.out.println("Deporte: " + deporte.getClass().getSimpleName());
+            System.out.println("Número de jugadores: " + deporte.getNumeroJugadores());
+            System.out.println("Número de árbitros: " + deporte.getNumeroArbitros());
+            System.out.println("Descripción: " + deporte.getDescripcion());
+            System.out.println();
+        }
 }
+
+
+
 
 
